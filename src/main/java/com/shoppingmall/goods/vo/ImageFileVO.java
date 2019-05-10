05182632a9e59f76cf5c247ff72a997749b76ef4
일반 <@DESKTOP-I43I40G>
2019-05-10 12:57:58 +0900

    수정

---
+++ src/main/java/com/shoppingmall/goods/vo/ImageFileVO.java
@@ -0,0 +1,61 @@
+package com.shoppingmall.goods.vo;
+
+import java.util.Date;
+
+public class ImageFileVO {
+	private int image_id;
+	private int goods_id;
+	private String filename;
+	private String reg_id;
+	private String filetype;
+	private Date reg_date;
+
+	public int getImage_id() {
+		return image_id;
+	}
+
+	public void setImage_id(int image_id) {
+		this.image_id = image_id;
+	}
+
+	public int getGoods_id() {
+		return goods_id;
+	}
+
+	public void setGoods_id(int goods_id) {
+		this.goods_id = goods_id;
+	}
+
+	public String getFilename() {
+		return filename;
+	}
+
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+
+	public String getReg_id() {
+		return reg_id;
+	}
+
+	public void setReg_id(String reg_id) {
+		this.reg_id = reg_id;
+	}
+
+	public String getFiletype() {
+		return filetype;
+	}
+
+	public void setFiletype(String filetype) {
+		this.filetype = filetype;
+	}
+
+	public Date getReg_date() {
+		return reg_date;
+	}
+
+	public void setReg_date(Date reg_date) {
+		this.reg_date = reg_date;
+	}
+
+}
