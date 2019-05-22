691d78a3b4a5462c584f0473a27c9605ff74838b
일반 <@DESKTOP-I43I40G>
2019-05-22 13:02:50 +0900

    Revert "Revert "장바구니 SQL Mapper,""
    This reverts commit 15a1c05ee5ad7744d1eccf4ebbfccd371898deb4.

---
+++ src/main/java/com/shoppingmall/cart/controller/CartContollerImpl.java
@@ -0,0 +1,92 @@
+package com.shoppingmall.cart.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.shoppingmall.cart.service.CartService;
+import com.shoppingmall.cart.vo.CartVO;
+import com.shoppingmall.common.base.BaseController;
+import com.shoppingmall.member.vo.MemberVO;
+
+public class CartContollerImpl extends BaseController implements CartController{
+	@Autowired
+	CartService cartService;
+	@Autowired
+	CartVO cartVO;
+	@Autowired
+	MemberVO memberVO;	
+
+	@RequestMapping(value="/myCartList.do", method=RequestMethod.GET)
+	public ModelAndView myCartList(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String viewName = (String)request.getAttribute("viewName");
+		ModelAndView mav = new ModelAndView(viewName);
+		HttpSession session = request.getSession();
+		MemberVO memberVO = (MemberVO)session.getAttribute("memberInfo");
+		String member_id = memberVO.getMember_id();
+		cartVO.setMember_id(member_id);
+		Map<String, List> cartMap = cartService.myCartList(cartVO);
+		session.setAttribute("cartMap", cartMap);
+		return mav;
+	}
+
+	@RequestMapping(value="/addGoodsInCart.do", method=RequestMethod.POST, produces="application/test; charset=utf8")
+	public @ResponseBody String addGoodsInCart(@RequestParam("goods_id")int goods_id, HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		HttpSession session = request.getSession();
+		memberVO = (MemberVO)session.getAttribute("memberInfo");
+		String member_id = memberVO.getMember_id();
+		
+		cartVO.setMember_id(member_id);
+		
+		cartVO.setGoods_id(goods_id);
+		cartVO.setMember_id(member_id);
+		boolean isAreadyExisted = cartService.findCartGoods(cartVO);
+		System.out.println("isAreadyExisted:" + isAreadyExisted);
+		if(isAreadyExisted == true) {
+			return "already_existed";
+		} else {
+			cartService.addGoodsInCart(cartVO);
+			return "add_success";
+		}
+	}
+
+	@RequestMapping(value="/modifyCartQty.do", method = RequestMethod.POST)
+	public @ResponseBody String modifyCartQty(@RequestParam("goods_id")int goods_id, 
+											  @RequestParam("cart_goods_qty")int cart_goods_qty, 
+											  HttpServletRequest request, HttpServletResponse response) throws Exception {
+		HttpSession session = request.getSession();
+		memberVO = (MemberVO)session.getAttribute("memberInfo");
+		String member_id = memberVO.getMember_id();
+		cartVO.setMember_id(member_id);
+		cartVO.setGoods_id(goods_id);
+		cartVO.setCart_goods_qty(cart_goods_qty);
+		boolean result = cartService.modifyCartQty(cartVO);
+		
+		if(result == true) {
+			return "modify_success";
+		} else {
+			return "modify_failed";
+		}
+	}
+
+	@RequestMapping(value="/removeCartGoods.do", method=RequestMethod.POST)
+	public ModelAndView removeCartGoods(@RequestParam("cart_id")int cart_id, HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		ModelAndView mav = new ModelAndView();
+		cartService.removeCartGoods(cart_id);
+		mav.setViewName("redirect:cart/myCartList.do");
+		return mav;
+	}
+	
+}
---
+++ src/main/java/com/shoppingmall/cart/service/CartService.java
@@ -0,0 +1,14 @@
+package com.shoppingmall.cart.service;
+
+import java.util.List;
+import java.util.Map;
+
+import com.shoppingmall.cart.vo.CartVO;
+
+public interface CartService {
+	public Map<String, List> myCartList(CartVO cartVO) throws Exception;
+	public boolean findCartGoods(CartVO cartVO) throws Exception;
+	public void addGoodsInCart(CartVO cartVO) throws Exception;
+	public boolean modifyCartQty(CartVO cartVO) throws Exception;
+	public void removeCartGoods(int cart_id) throws Exception;
+}
