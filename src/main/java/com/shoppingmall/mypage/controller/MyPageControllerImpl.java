c3fea9737958fbf477e178dd7c6d0db350501694
zidol <zidol@192.168.2.66>
2019-05-29 23:30:26 +0900

    회원 정보 수정

--- src/main/java/com/shoppingmall/mypage/controller/MyPageControllerImpl.java
+++ src/main/java/com/shoppingmall/mypage/controller/MyPageControllerImpl.java
@@ -130,10 +130,9 @@ public class MyPageControllerImpl extends BaseController  implements MyPageContr
 		String  member_id=memberVO.getMember_id();
 		if(attribute.equals("member_birth")){
 			val=value.split(",");
-			memberMap.put("member_birth_y",val[0]);
-			memberMap.put("member_birth_m",val[1]);
-			memberMap.put("member_birth_d",val[2]);
-			memberMap.put("member_birth_gn",val[3]);
+			memberMap.put("birth_year",val[0]);
+			memberMap.put("birth_month",val[1]);
+			memberMap.put("birth_day",val[2]);
 		}else if(attribute.equals("tel")){
 			val=value.split(",");
 			memberMap.put("tel1",val[0]);
@@ -144,18 +143,18 @@ public class MyPageControllerImpl extends BaseController  implements MyPageContr
 			memberMap.put("hp1",val[0]);
 			memberMap.put("hp2",val[1]);
 			memberMap.put("hp3",val[2]);
-			memberMap.put("smssts_yn", val[3]);
+			memberMap.put("sms_yn", val[3]);
 		}else if(attribute.equals("email")){
 			val=value.split(",");
 			memberMap.put("email1",val[0]);
 			memberMap.put("email2",val[1]);
-			memberMap.put("emailsts_yn", val[2]);
+			memberMap.put("email_yn", val[2]);
 		}else if(attribute.equals("address")){
 			val=value.split(",");
 			memberMap.put("zipcode",val[0]);
-			memberMap.put("roadAddress",val[1]);
-			memberMap.put("jibunAddress", val[2]);
-			memberMap.put("namujiAddress", val[3]);
+			memberMap.put("road_address",val[1]);
+			memberMap.put("jibun_address", val[2]);
+			memberMap.put("detail_address", val[3]);
 		}else {
 			memberMap.put(attribute,value);	
 		}
